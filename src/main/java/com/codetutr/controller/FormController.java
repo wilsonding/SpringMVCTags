16d1d12cfdd75ac2be29fce236ef90267a61ecee
wilson <dosurely@gmail.com>
2014-07-13 21:41:30 -0400

    first commit for spring mvc form tags

---
+++ src/main/java/com/codetutr/controller/FormController.java
@@ -0,0 +1,33 @@
+package com.codetutr.controller;
+
+import com.codetutr.form.Subscriber;
+import com.codetutr.form.Subscriber.Frequency;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by wilson on 7/12/2014.
+ */
+@Controller
+public class FormController {
+    @ModelAttribute("frequencies")
+    public Frequency[] frequencies(){
+        return Frequency.values();
+    }
+
+    @RequestMapping(value = "form", method = RequestMethod.GET)
+    public String loadFormPage(Model m){
+        m.addAttribute("subscriber",new Subscriber());
+        return "formPage";
+    }
+
+    @RequestMapping(value = "form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute Subscriber subscriber, Model m){
+        m.addAttribute("message", "Successfully saved person: " + subscriber.toString());
+        return "formPage";
+    }
+
+}
